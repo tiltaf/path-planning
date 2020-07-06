8e633080d5dcd742afffcf9fe9090f4dfa08e592
tiltaf <12beetiltaf@seecs.edu.pk>
2020-07-06 22:56:23 +0500

    name of class A* changed to Astar and names of variable modified to make code more readable

--- Path_planning.py
+++ Path_planning.py
@@ -1,4 +1,6 @@
-class A*:
+import numpy as np
+import math
+class Astar:
     '''A star path planning class to find optimal path given start point  and
     goal point in a map_grid using A* algorithm
 
@@ -11,6 +13,16 @@ class A*:
         self.map_grid = map_grid
         self.start_point = endpoints[0]
         self.goal = endpoints[1]
+        self.step_cost = 2
+        self.delta = [[-1, 0],  # go up
+                 [0, -1],  # go left
+                 [1, 0],  # go down
+                 [0, 1]]  # go right
+        self.neig = [[0.1, 0.25, 0.25, 0.25, 0.1],  # go up
+                [0.25, 0.5, 0.5, 0.5, 0.25],  # go left
+                [0.5, 0.5, 1, 0.5, 0.5],  # go down
+                [0.25, 0.5, 0.5, 0.5, 0.25],
+                [0.1, 0.25, 0.25, 0.25, 0.1]]
     def heuristic(self,x,y):
         '''function to give rough estimate cost of goal/destination point from given
         point using euclidean distance
@@ -39,10 +51,10 @@ class A*:
             for j in range(-2,2):
                 y2 = y + j
                 if x2 >= 0 and x2 < len(self.map_grid) and y2 >=0 and y2 <len(self.map_grid[0]):
-                    cost = cost + 2*self.map_grid[x2][y2]*neig[2+i][2+j]
+                    cost = cost + 2*self.map_grid[x2][y2]*self.neig[2+i][2+j]
         return cost
 
-    def path_find():
+    def path_find(self):
         '''function to populate the possible path and find best path
 
         Args:
@@ -50,63 +62,60 @@ class A*:
         return:
             best path between given two points'''
         def get_optimum():
-            x = goal[0]
-            y = goal[1]
-            policy[x][y] = 5
+            x = self.goal[0]
+            y = self.goal[1]
             expand[x][y] = -50
             print(action[x][y])
-            while x != init[0] or y != init[1]:
-                  x2 = x - delta[action[x][y]][0]
-                  y2 = y - delta[action[x][y]][1]
+            while x != self.start_point[0] or y != self.start_point[1]:
+                  x2 = x - self.delta[action[x][y]][0]
+                  y2 = y - self.delta[action[x][y]][1]
                   expand[x2][y2] = 500
-                  policy[x][y] = action[x][y]
                   x = x2
                   y = y2
                   path.append([x,y])
         path = []
-        closed = [[0 for row in range(len(self.map_grid[0]))] for col in range(len(self.map_grid))]
-        closed[init[0]][init[1]] = 1
+        closed_list = [[0 for row in range(len(self.map_grid[0]))] for col in range(len(self.map_grid))]
+        closed_list[self.start_point[0]][self.start_point[1]] = 1
         expand = [[-1 for row in range(len(self.map_grid[0]))] for col in range(len(self.map_grid)) ]
         action = [[-1 for row in range(len(self.map_grid[0]))] for col in range(len(self.map_grid))]
-        policy = [[-1 for row in range(len(self.map_grid[0]))] for col in range(len(self.map_grid))]
-        x = init[0]
-        y = init[1]
-        g = 0
-        h = heuristic(x,y) + neighCost(x,y)
-        f = g + h
-        open = [[f, g, h, x, y]]
+        x = self.start_point[0]
+        y = self.start_point[1]
+        prev_cost = 0
+        heuristic_cost = self.heuristic(x,y) + self.neighCost(x,y)
+        updated_cost = prev_cost + heuristic_cost
+        open_list = [[updated_cost, prev_cost, x, y]]
         found = False # flag that is set when search is complete
         resign = False # flag set if we can't find expand
         count = 0
 
         while not found and not resign:
-            if len(open) == 0:
+            if len(open_list) == 0:
                         resign = True
                         print('resign')
             else:
-                open.sort()
-                open.reverse()
-                next = open.pop()
-                x = next[3]
-                y = next[4]
-                g = next[1]
+                open_list.sort()
+                open_list.reverse()
+                next = open_list.pop()
+                x = next[2]
+                y = next[3]
+                prev_cost = next[1]
                 expand[x][y] = count
                 count += 1
-            if x == goal[0] and y == goal[1]:
+            if x == self.goal[0] and y == self.goal[1]:
                         found = True
 #                        print('found yar')
             else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
+                for i in range(len(self.delta)):
+                    x2 = x + self.delta[i][0]
+                    y2 = y + self.delta[i][1]
                     if x2 >= 0 and x2 < len(self.map_grid) and y2 >=0 and y2 <len(self.map_grid[0]):
-                        if closed[x2][y2] == 0 and self.map_grid[x2][y2] == 0:
-                            g2 = g + cost
-                            h2 = heuristic(x2,y2) + neighCost(x2,y2)
-                            f2 = g2 + h2
-                            open.append([f2, g2, h2, x2, y2])
-                            closed[x2][y2] = 1
+                        if closed_list[x2][y2] == 0 and self.map_grid[x2][y2] == 0:
+                            updated_cost = prev_cost + self.step_cost
+                            heuristic_cost = self.heuristic(x2,y2) + self.neighCost(x2,y2)
+                            total_cost = updated_cost + heuristic_cost
+                            open_list.append([total_cost, updated_cost, x2, y2])
+                            closed_list[x2][y2] = 1
                             action[x2][y2] = i
-
+        get_optimum()
 
         return path
